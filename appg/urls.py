cf2830728356bfa7f5eed95e78a16993a8a8b5ea
ogGEORGE-x <gfurious01@gmail.com>
2026-03-30 08:21:31 -0400

    Index modified

--- appg/urls.py
+++ appg/urls.py
@@ -3,5 +3,18 @@ from appg import views
 
 urlpatterns={
     path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('shop/',views.shop,name='shop'),
+    path('contact/',views.contact,name='contact'),
+    path('news/',views.news,name='news'),
+    path('single-news/',views.news,name='single-news'),
+    path('checkout/',views.shop,name='checkout'),
+    path('single-product/',views.shop,name='single-product'),
+    path('cart/',views.shop,name='cart'),
+    path('404/',views.shop,name='404'),
+    path('index2/',views.home,name='index2'),
+    path('index3/',views.home,name='index3'),
+
+
 
 }
--- appg/views.py
+++ appg/views.py
@@ -3,3 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def shop(request):
+    return render(request,'shop.html')
+def contact(request):
+    return render(request,'contact.html')
+def news(request):
+    return render(request,'news.html')
+def news_detail(request,id):
+    return render(request,'news_detail.html',{'id':id})
+def news_create(request):
+    return render(request,'news_create.html')
+def news_update(request,id):
+    return render(request,'news_update.html',{'id':id})
+def news_delete(request,id):
+    return render(request,'news_delete.html',{'id':id})
+def single_product(request):
+    return render(request,'single-product.html')
+def checkout(request):
+    return render(request,'checkout.html')
+def cart(request):
+    return render(request,'cart.html')
+def error_404(request,exception):
+    return render(request,'404.html',status=404)
+
